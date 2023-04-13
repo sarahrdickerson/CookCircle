0b17b56310e40367cd8637aded861a9a861e6391
sarahrdickerson <sarahrdickerson@gmail.com>
2023-04-13 02:23:31 -0500

    firebase config working (?)

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import SignIn from './components/SignIn';
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app! Hi! hi!</Text>
+      <SignIn />
       <StatusBar style="auto" />
     </View>
   );
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cook Circle
+
+## Tech Stack
+
+1. React Native Expo
+2. NativeWind and TailwindCSS
+3. Firebase
